75c7a3377d0350af154338e2df37dd89e9f01871
DidierHerlin <didierherlin18@gmail.com>
2025-04-08 11:57:49 +0300

    Sauvegarde de mes modifications avant de changer de branche

--- backends/vonjyapp/views.py
+++ backends/vonjyapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Commune
+from .serializers import CommuneSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class CommuneViewSet(viewsets.ModelViewSet):
+    serializer_class = CommuneSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Commune.objects.all()
+        nom_commune = self.request.query_params.get('nomCommune', None)
+        if nom_commune:
+            queryset = queryset.filter(nomCommune__icontains=nom_commune)  # icontains = insensible à la casse
+        return queryset
+
